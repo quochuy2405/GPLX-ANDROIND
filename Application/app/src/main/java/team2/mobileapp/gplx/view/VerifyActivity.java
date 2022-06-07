c73137b263b45f84c2fa57cae050454090e12459
Bùi Quốc Huy <huy01682312920@gmail.com>
2022-06-07 09:17:03 +0700

    update v2

--- Application/app/src/main/java/team2/mobileapp/gplx/view/VerifyActivity.java
+++ Application/app/src/main/java/team2/mobileapp/gplx/view/VerifyActivity.java
@@ -2,9 +2,15 @@ package team2.mobileapp.gplx.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import team2.mobileapp.gplx.R;
 import team2.mobileapp.gplx.Retrofit.api.SMSAPI;
@@ -13,33 +19,49 @@ public class VerifyActivity extends AppCompatActivity {
 
     EditText etVerifyCode;
     Button btnVerifyCode;
+    RelativeLayout checkOutFocus;
     SMSAPI sms = new SMSAPI();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_bottom);
+        checkOutFocus = findViewById(R.id.body_verify);
         setContentView(R.layout.activity_verify);
+        checkOutFocus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hideKeyboard();
+            }
+        });
+        etVerifyCode = findViewById(R.id.et_verify_code);
+        btnVerifyCode = findViewById(R.id.btn_verify);
 
-//        et_verify_code = findViewById(R.id.et_verify_code);
-//        btn_verify = findViewById(R.id.btn_verify);
-//
-//        Intent setNewPass = new Intent(this, SetNewPasswordActivity.class);
-//
-//        Verify(setNewPass);
+        Intent setNewPass = new Intent(this, SetNewPasswordActivity.class);
+
+        Verify(setNewPass);
     }
 
-//    private void Verify(Intent setNewPass) {
-//        btn_verify.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if(et_verify_code.getText().toString().isEmpty())
-//                    Toast.makeText(VerifyActivity.this, "Please enter verification code", Toast.LENGTH_LONG).show();
-//                else{
+    private void Verify(Intent setNewPass) {
+        btnVerifyCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hideKeyboard();
+                if(btnVerifyCode.getText().toString().isEmpty())
+                   Toast .makeText(VerifyActivity.this, "Please enter verification code", Toast.LENGTH_LONG).show();
+                else{
 //                    startActivity(setNewPass);
-//                }
-//            }
-//        });
-//    }
+                }
+            }
+        });
+    }
+public void hideKeyboard() {
+    View view = this.getCurrentFocus();
+    if(view  !=null){
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+}
 @Override
 public void onBackPressed() {
     super.onBackPressed();
